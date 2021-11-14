a937552c8cf5d6099ffac791777539fd513cf087
yiscah <yiscahls@armosec.io>
2021-11-14 14:53:57 +0200

    add test for inspectmap

--- workloadinterface/interface_test.go
+++ workloadinterface/interface_test.go
@@ -11,3 +11,20 @@ func TestIWorkload(t *testing.T) {
 	a = NewWorkloadMock(nil)
 	fmt.Printf("%v", a.GetName())
 }
+
+func TestInspectMap(t *testing.T) {
+	mapexample := map[string]interface{}{"name": "a",
+		"relatedObjects": struct {
+			role        string
+			rolebinding string
+		}{"role-a", "rolebinding-b"},
+		"namespace": "c",
+	}
+	if name, ok := InspectMap(mapexample, "name"); ok {
+		if name != "a" {
+			t.Errorf("error in inspectMap, name %s != 'a'", name)
+		}
+	} else {
+		t.Errorf("error in inspectMap, couldn't find name: '%s", name)
+	}
+}
