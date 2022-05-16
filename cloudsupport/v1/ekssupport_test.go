f358fc414e16374c1de5765fe6f1e4c6c79d6ec0
DanielGrunbergerCA <danielg@armosec.io>
2022-05-16 14:24:19 +0300

    unit tests for eks

---
+++ cloudsupport/v1/ekssupport_test.go
@@ -0,0 +1,27 @@
+package v1
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetContextName(t *testing.T) {
+	mockname1 := "arn:aws:eks:eu-north-1:123456789:cluster-test-cluster"
+	eksSupport := NewEKSSupport()
+	name := eksSupport.GetContextName(mockname1)
+	assert.Equal(t, "test-cluster", name)
+	region, err := eksSupport.GetRegion(mockname1)
+	assert.NoError(t, err)
+	assert.Equal(t, "eu-north-1", region)
+
+	mockname2 := "arn:aws:eks:eu-north-1:123456789:cluster/test-cluster"
+	splittedCluster := strings.Split(mockname2, "/")
+	name = splittedCluster[len(splittedCluster)-1]
+	assert.Equal(t, "test-cluster", name)
+	region, err = eksSupport.GetRegion(mockname2)
+	assert.NoError(t, err)
+	assert.Equal(t, "eu-north-1", region)
+
+}
