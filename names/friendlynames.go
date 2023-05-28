2426ad360b8543422adab8e9955a49f947fdaed1
Vlad Klokun <vklokun@protonmail.ch>
2023-05-28 20:34:10 +0300

    feat: validate friendly names
    Signed-off-by: Vlad Klokun <vklokun@protonmail.ch>

--- names/friendlynames.go
+++ names/friendlynames.go
@@ -2,29 +2,71 @@ package names
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
 const (
-	// friendlyNameInstanceIDFormat is a format of the friendly name string
-	friendlyNameInstanceIDFormat     = "%s-%s-%s-%s-%s"
+	// friendlyNameInstanceIDHashlessFormat is a format of the friendly name string without hash-based identifiers
+	friendlyNameInstanceIDHashlessFormat = "%s-%s-%s"
+	// friendlyNameInstanceIDFormat is a format of the friendly name string:
+	// "hashlessFormat + hashLeading + hashTrailing"
+	friendlyNameInstanceIDFormat     = "%s-%s-%s"
 	friendlyNameInstanceIDHashLength = 4
 
 	// friendlyNameImageIDHashLength is the length of the hash suffix to append to the friendly name
 	friendlyNameImageIDFormat     = "%s-%s"
 	friendlyNameImageIDHashLength = 6
+
+	maxDNSSubdomainLength = 253
+	maxImageNameLength    = maxDNSSubdomainLength - friendlyNameImageIDHashLength - 1
 )
 
 // imageToDNSSubdomainReplacer is a replacer that can replace a valid, well-formed container image string to a valid DNS Subdomain
 var imageToDNSSubdomainReplacer = strings.NewReplacer("://", "-", ":", "-", "/", "-")
 
-func isInvalidFriendlyName(s string) bool {
-	return strings.ContainsAny(s, "/:")
+var (
+	dnsSubdomainRegexp = regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9.-]{0,251}[a-zA-Z0-9]$`)
+	dnsLabelRegexp     = regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]$`)
+)
+
+// IsValidDNSSubdomainName returns true if a given string is a valid DNS Subdomain name as defined in the Kubernetes docs
+func IsValidDNSSubdomainName(s string) bool {
+	return dnsSubdomainRegexp.MatchString(s)
 }
 
-// sanitizeImage returns a sanitized string safe for use with K8s names
+// IsValidDNSLabelName returns true if a given string is a valid DNS label name as defined in the Kubernetes docs
+func IsValidDNSLabelName(s string) bool {
+	return dnsLabelRegexp.MatchString(s)
+}
+
+// IsValidFriendlyName returns true if a given string is a valid friendly name, else false
+//
+// A string is considered a valid friendly name if it can be used as a name of a Kubernetes resource
+func IsValidFriendlyName(s string) bool {
+	return IsValidDNSSubdomainName(s)
+}
+
+// sanitizeImage returns a sanitized image string safe for use with K8s names
+//
+// It expects a valid image name string
 func sanitizeImage(image string) string {
-	return imageToDNSSubdomainReplacer.Replace(image)
+	sanitized := imageToDNSSubdomainReplacer.Replace(image)
+
+	if len(image) >= maxImageNameLength {
+		sanitized = sanitized[:maxImageNameLength]
+	}
+	return sanitized
+}
+
+// sanitizeInstanceIDSlug returns a sanitized instance ID slug
+func sanitizeInstanceIDSlug(instanceIDSlug string) string {
+	if len(instanceIDSlug) > 243 {
+		return instanceIDSlug[:243]
+	} else {
+		return instanceIDSlug
+	}
+
 }
 
 // InstanceIDToFriendlyName retuns a human-friendly name representation given a description of an instance ID
@@ -33,10 +75,13 @@ func sanitizeImage(image string) string {
 func InstanceIDToFriendlyName(name, namespace, kind, hashedID string) (string, error) {
 	leadingDigest, trailingDigest := hashedID[:friendlyNameInstanceIDHashLength], hashedID[len(hashedID)-friendlyNameInstanceIDHashLength:]
 
-	var err error
-	friendlyName, err := fmt.Sprintf(friendlyNameInstanceIDFormat, namespace, kind, name, leadingDigest, trailingDigest), nil
+	hashlessInstanceIDSlug := fmt.Sprintf(friendlyNameInstanceIDHashlessFormat, namespace, kind, name)
+	hashlessInstanceIDSlug = sanitizeInstanceIDSlug(hashlessInstanceIDSlug)
 
-	if isInvalidFriendlyName(friendlyName) {
+	var err error
+	friendlyName, err := fmt.Sprintf(friendlyNameInstanceIDFormat, hashlessInstanceIDSlug, leadingDigest, trailingDigest), nil
+
+	if !IsValidFriendlyName(friendlyName) {
 		friendlyName, err = "", ErrInvalidFriendlyName
 	}
 
@@ -56,7 +101,7 @@ func ImageInfoToFriendlyName(image, imageHash string) (string, error) {
 	sanitizedImage := sanitizeImage(image)
 	friendlyName, err := fmt.Sprintf(friendlyNameImageIDFormat, sanitizedImage, imageHashStub), nil
 
-	if isInvalidFriendlyName(friendlyName) {
+	if !IsValidFriendlyName(friendlyName) {
 		friendlyName, err = "", ErrInvalidFriendlyName
 	}
 
--- names/friendlynames_test.go
+++ names/friendlynames_test.go
@@ -77,6 +77,13 @@ func TestImageInfoToFriendlyName(t *testing.T) {
 			"",
 			ErrInvalidFriendlyName,
 		},
+		{
+			"Image names that would produce overflowing friendly names should get truncated to limit",
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc",
+			"f4e3b6489888647ce1834b601c6c06b9f8c03dee6e097e13ed3e28c01ea3ac8c",
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab-a3ac8c",
+			nil,
+		},
 	}
 
 	for _, tc := range tt {
@@ -118,6 +125,15 @@ func TestInstanceIDToFriendlyName(t *testing.T) {
 			wantErr:        nil,
 		},
 		{
+			name:           "instanceID that produces overflowing friendly name gets truncated to limit",
+			inputNamespace: "0123456789",
+			inputKind:      "0123456789",
+			inputName:      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+			inputHashedID:  "1ba506b28f9ee9c7e8a0c98840fe5a1fe21142d225ecc526fbb535d0d6344aaf",
+			want:           "0123456789-0123456789-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-1ba5-4aaf",
+			wantErr:        nil,
+		},
+		{
 			name:           "invalid instanceID produces matching error",
 			inputNamespace: "default",
 			inputKind:      "Service",
@@ -134,6 +150,148 @@ func TestInstanceIDToFriendlyName(t *testing.T) {
 
 			assert.Equal(t, tc.want, got)
 			assert.ErrorIs(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestIsValidSubdomainName(t *testing.T) {
+	tt := []struct {
+		name      string
+		inputName string
+		want      bool
+	}{
+		{
+			name:      "Short alphanumeric name is considered valid",
+			inputName: "nginx",
+			want:      true,
+		},
+		{
+			name:      "Colon character should render the string invalid",
+			inputName: "n:ginx",
+			want:      false,
+		},
+		{
+			name:      "Slash character should render the string invalid",
+			inputName: "n/ginx",
+			want:      false,
+		},
+		{
+			name:      "Caret character should render the string invalid",
+			inputName: "n^ginx",
+			want:      false,
+		},
+		{
+			name:      "Empty string should be considered invalid",
+			inputName: "",
+			want:      false,
+		},
+		{
+			name:      "Periods should be allowed",
+			inputName: "docker.io",
+			want:      true,
+		},
+		{
+			name:      "Hyphens should be allowed",
+			inputName: "web-app",
+			want:      true,
+		},
+		{
+			name:      "Numbers should be allowed",
+			inputName: "webapp1",
+			want:      true,
+		},
+		{
+			name:      "Names starting from an allowed non-alphanumeric character should be invalid",
+			inputName: "-webapp",
+			want:      false,
+		},
+		{
+			name:      "Names ending with an allowed non-alphanumeric character should be invalid",
+			inputName: "webapp-",
+			want:      false,
+		},
+		{
+			name:      "Names over 253 characters should be invalid",
+			inputName: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX",
+			want:      false,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := IsValidDNSSubdomainName(tc.inputName)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestIsValidDSNLabelName(t *testing.T) {
+	tt := []struct {
+		name      string
+		inputName string
+		want      bool
+	}{
+		{
+			name:      "Short alphanumeric name is considered valid",
+			inputName: "nginx",
+			want:      true,
+		},
+		{
+			name:      "Colon character should render the string invalid",
+			inputName: "n:ginx",
+			want:      false,
+		},
+		{
+			name:      "Slash character should render the string invalid",
+			inputName: "n/ginx",
+			want:      false,
+		},
+		{
+			name:      "Caret character should render the string invalid",
+			inputName: "n^ginx",
+			want:      false,
+		},
+		{
+			name:      "Empty string should be considered invalid",
+			inputName: "",
+			want:      false,
+		},
+		{
+			name:      "Periods should NOT be allowed",
+			inputName: "docker.io",
+			want:      false,
+		},
+		{
+			name:      "Hyphens should be allowed",
+			inputName: "web-app",
+			want:      true,
+		},
+		{
+			name:      "Numbers should be allowed",
+			inputName: "webapp1",
+			want:      true,
+		},
+		{
+			name:      "Names starting from an allowed non-alphanumeric character should be invalid",
+			inputName: "-webapp",
+			want:      false,
+		},
+		{
+			name:      "Names ending with an allowed non-alphanumeric character should be invalid",
+			inputName: "webapp-",
+			want:      false,
+		},
+		{
+			name:      "Names over 63 characters should be invalid",
+			inputName: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaX",
+			want:      false,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := IsValidDNSLabelName(tc.inputName)
+			assert.Equal(t, tc.want, got)
 		})
 	}
 }
