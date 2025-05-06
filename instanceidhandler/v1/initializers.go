6b46d25ecdeb8b88a2b49879752b12a40530805d
Matthias Bertschy <matthias.bertschy@gmail.com>
2025-05-06 16:18:05 +0200

    fix GenerateInstanceID for ds, add jsonPath escaping
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- instanceidhandler/v1/initializers.go
+++ instanceidhandler/v1/initializers.go
@@ -5,9 +5,12 @@ import (
 	"hash"
 	"hash/fnv"
 	"math"
+	"reflect"
 	"strconv"
 	"strings"
 
+	"github.com/kubescape/go-logger"
+	"github.com/kubescape/go-logger/helpers"
 	"github.com/kubescape/k8s-interface/instanceidhandler"
 	"github.com/kubescape/k8s-interface/instanceidhandler/v1/containerinstance"
 	"github.com/kubescape/k8s-interface/workloadinterface"
@@ -17,6 +20,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/dump"
 	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/client-go/util/jsonpath"
 )
 
 const (
@@ -26,7 +30,9 @@ const (
 )
 
 // GenerateInstanceID generates instance ID from workload
-func GenerateInstanceID(w workloadinterface.IWorkload) ([]instanceidhandler.IInstanceID, error) {
+// Pods created by a CronJob have an alternate naming convention, using a calculated pod template hash,
+// jsonPaths can be specified to drop fields from the pod spec before hashing.
+func GenerateInstanceID(w workloadinterface.IWorkload, jsonPaths []string) ([]instanceidhandler.IInstanceID, error) {
 	var templateHash string
 	if podHash, ok := w.GetLabel("pod-template-hash"); ok && podHash != "" {
 		templateHash = podHash
@@ -39,10 +45,16 @@ func GenerateInstanceID(w workloadinterface.IWorkload) ([]instanceidhandler.IIns
 	var alternateName string
 	if len(ownerReferences) > 0 {
 		ownerReference = &ownerReferences[0]
-		// if the Pod is created by a CronJob, its parent is a Job named after the CronJob
-		// with the scheduled timestamp appended to it (unix time in minutes).
-		// https://github.com/kubernetes/kubernetes/blob/master/pkg/controller/cronjob/utils.go#L277
-		if ownerReference.Kind == "Job" {
+		switch ownerReference.Kind {
+		case "DaemonSet":
+			if label, ok := w.GetLabel(appsv1.StatefulSetRevisionLabel); ok {
+				alternateName = ownerReference.Name + "-" + label
+			}
+		case "Job":
+			// if the Pod is created by a CronJob, its parent is a Job named after the CronJob
+			// with the scheduled timestamp appended to it (unix time in minutes).
+			// https://github.com/kubernetes/kubernetes/blob/master/pkg/controller/cronjob/utils.go#L277
+			// TODO add a json path to exclude some fields
 			s := strings.Split(ownerReference.Name, "-")
 			if len(s) > 1 && isUnixTimeInMinutes(s[len(s)-1]) {
 				// calculate pod template hash
@@ -52,12 +64,12 @@ func GenerateInstanceID(w workloadinterface.IWorkload) ([]instanceidhandler.IIns
 				if err != nil {
 					return nil, fmt.Errorf("failed to get pod spec: %v", err)
 				}
-				DeepHashObject(podTemplateSpecHasher, spec)
+				DeepHashObject(podTemplateSpecHasher, spec, jsonPaths)
 				templateHash = rand.SafeEncodeString(fmt.Sprint(podTemplateSpecHasher.Sum32()))
 				s[len(s)-1] = templateHash
 				alternateName = strings.Join(s, "-")
 			}
-		} else if ownerReference.Kind == "StatefulSet" {
+		case "StatefulSet":
 			if label, ok := w.GetLabel(appsv1.StatefulSetRevisionLabel); ok {
 				alternateName = label
 			}
@@ -99,10 +111,17 @@ func GenerateInstanceID(w workloadinterface.IWorkload) ([]instanceidhandler.IIns
 	return convertContainersToIInstanceID(c), nil
 }
 
-func DeepHashObject(hasher hash.Hash32, pod *core1.PodSpec) {
+func DeepHashObject(hasher hash.Hash32, pod *core1.PodSpec, extraJsonPaths []string) {
 	// sanitize pod sped
 	dropProjectedVolumesAndMounts(pod)
-	dropHostname(pod)
+	jsonPaths := []string{
+		".hostname",
+		".containers[*].env[?(@.name==\"DD_INJECT_START_TIME\")]",
+	}
+	jsonPaths = append(jsonPaths, extraJsonPaths...)
+	if err := dropFieldsByJSONPath(pod, jsonPaths); err != nil {
+		logger.L().Error("failed to drop fields by JSONPath", helpers.Error(err), helpers.Interface("jsonPath", jsonPaths))
+	}
 	hasher.Reset()
 	_, _ = fmt.Fprintf(hasher, "%v", dump.ForHash(pod))
 }
@@ -141,9 +160,31 @@ func dropProjectedVolumesAndMounts(podSpec *core1.PodSpec) {
 	}
 }
 
-// https://github.com/kubernetes/autoscaler/blob/5077f4817bc3105bd61659950833bd6d66edc556/cluster-autoscaler/utils/utils.go#L109
-func dropHostname(podSpec *core1.PodSpec) {
-	podSpec.Hostname = ""
+func dropFieldsByJSONPath(podSpec *core1.PodSpec, jsonPaths []string) error {
+	for _, path := range jsonPaths {
+		j := jsonpath.New("dropFields")
+		if err := j.Parse(fmt.Sprintf("{%s}", path)); err != nil {
+			return fmt.Errorf("failed to parse JSONPath %s: %v", path, err)
+		}
+		results, err := j.FindResults(podSpec)
+		if err != nil {
+			return fmt.Errorf("failed to find results for JSONPath %s: %v", path, err)
+		}
+		for _, values := range results {
+			for i := range values {
+				v := values[i]
+				if v.CanSet() {
+					// Get the zero value for the element's type
+					zeroVal := reflect.Zero(v.Type())
+					// Set the element to its zero value
+					v.Set(zeroVal)
+				} else {
+					fmt.Printf("Element at index %d is not settable\n", i)
+				}
+			}
+		}
+	}
+	return nil
 }
 
 func isUnixTimeInMinutes(s string) bool {
@@ -161,7 +202,7 @@ func GenerateInstanceIDFromPod(pod *core1.Pod) ([]instanceidhandler.IInstanceID,
 		return nil, fmt.Errorf("convert pod to unstructured: %v", err)
 	}
 	w := workloadinterface.NewWorkloadObj(unstructuredObj)
-	return GenerateInstanceID(w)
+	return GenerateInstanceID(w, nil)
 }
 
 // GenerateInstanceIDFromString generates instance ID from string
